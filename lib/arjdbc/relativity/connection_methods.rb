81d198e44bf444916c93fd7f81b71a7fcc98938b
Eric Cranston <cracell@gmail.com>
2016-12-08 15:26:18 -0800

    Quick fix for constantize bug ID:866

--- lib/arjdbc/relativity/connection_methods.rb
+++ lib/arjdbc/relativity/connection_methods.rb
@@ -6,8 +6,10 @@ class ActiveRecord::Base
       config[:driver] ||= 'relativity.jdbc.Driver'
       config[:adapter_spec] ||= ::ArJdbc::Relativity
       config[:adapter_class] = ActiveRecord::ConnectionAdapters::RelativityAdapter unless config.key?(:adapter_class)
-      if config.key?(:adapter_class)
+      if config.key?(:adapter_class) && config[:adapter_class].class == String
         config[:adapter_class] = config[:adapter_class].constantize
+      elsif config.key?(:adapter_class)
+        config[:adapter_class] = config[:adapter_class]
       else
         config[:adapter_class] = ActiveRecord::ConnectionAdapters::RelativityAdapter
       end
